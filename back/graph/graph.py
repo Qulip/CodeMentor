456ca3dea761c52a203911c56acea84db7bfcb27
Qulip <ilkwon.ryu@gmail.com>
2025-04-17 22:13:53 +0900

    feat: graph 구현

--- back/graph/graph.py
+++ back/graph/graph.py
@@ -1,21 +1,30 @@
 from langgraph.graph import StateGraph, END
 
 from core.state import AnswerState, AgentType
-from agent.input_agent import InputAgent, GeneratorAgent, AnalyzerAgent
+from agent.input_agent import InputAgent
+from agent.generator_agent import GeneratorAgent
+from agent.analyzer_agent import AnalyzerAgent
+from agent.retriever_agent import RetrieverAgent
+from agent.review_agent import ReviewerAgent
 
 
-# TODO: Agent 추가 및 node, edge 추가 필요
 def create_agent_graph(k: int = 3, session_id: str = ""):
 
     workflow = StateGraph(AnswerState)
 
-    input_agent = InputAgent(k=k, session_id=session_id)
+    input_agent = InputAgent(session_id=session_id)
     analyzer_agent = AnalyzerAgent(k=k, session_id=session_id)
-    generator_agent = GeneratorAgent(k=k, session_id=session_id)
+    generator_agent = GeneratorAgent(session_id=session_id)
+    retriever_agent = RetrieverAgent(k=k, session_id=session_id)
+    reviewer_agent = ReviewerAgent(session_id=session_id)
 
     workflow.add_node(AgentType.INPUT, input_agent.run)
     workflow.add_node(AgentType.ANALYZER, analyzer_agent.run)
     workflow.add_node(AgentType.GENERATOR, generator_agent.run)
+    workflow.add_node(AgentType.RETRIEVER, retriever_agent.run)
+    workflow.add_node(AgentType.REVIEWER, reviewer_agent.run)
+
+    workflow.set_entry_point(AgentType.INPUT)
 
     workflow.add_edge(AgentType.INPUT, AgentType.ANALYZER)
     workflow.add_edge(AgentType.ANALYZER, AgentType.GENERATOR)
@@ -27,9 +36,9 @@ def create_agent_graph(k: int = 3, session_id: str = ""):
     )
 
     workflow.add_edge(AgentType.RETRIEVER, AgentType.REVIEWER)
-    workflow.add_edge(AgentType.RETRIEVER, END)
+    workflow.add_edge(AgentType.REVIEWER, END)
 
-    return workflow.compile
+    return workflow.compile()
 
 
 def _is_solution_generate_end(state: AnswerState):
@@ -51,7 +60,3 @@ if __name__ == "__main__":
     output_path = "agent_graph.png"
     with open(output_path, "wb") as f:
         f.write(graph_image)
-
-    import subprocess
-
-    subprocess.run(["open", output_path])
