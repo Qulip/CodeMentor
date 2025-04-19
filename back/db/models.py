2230f4631a8fd6596c46ded3733937c7bc350e4c
Qulip <ilkwon.ryu@gmail.com>
2025-04-20 05:42:07 +0900

    fix: model 오타 수정

--- back/db/models.py
+++ back/db/models.py
@@ -1,6 +1,5 @@
-import datetime
-
 from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
 
 from db.database import Base
 
@@ -17,7 +16,7 @@ class Question(Base):
     - solutions: 예상 원인 솔루션 리스트
     - study_tips: 추가 학습 추천 내용
     - docs: RAG 검색 결과
-    - created_time: 생성 시간
+    - created_at: 생성 시간
     """
 
     __tablename__ = "questions"
@@ -33,4 +32,4 @@ class Question(Base):
     solutions = Column(Text, nullable=False)
     study_tips = Column(Text, nullable=False)
     docs = Column(Text, nullable=False)
-    created_time = Column(DateTime, default=datetime.datetime.now)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
