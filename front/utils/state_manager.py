0c0d453450581254048c22114680ebe1059611f4
Qulip <ilkwon.ryu@gmail.com>
2025-04-19 22:48:29 +0900

    feat: 미사용 메서드 제거

--- front/utils/state_manager.py
+++ front/utils/state_manager.py
@@ -2,23 +2,18 @@ import streamlit as st
 
 
 def init_session_state():
-    # 세션 스테이트 초기화
+    """
+    Streamlit 세션 스테이트 초기화
+    """
     if "app_mode" not in st.session_state:
         reset_session_state()
 
 
 def reset_session_state():
+    """
+    Streamlit 세션 스테이트 리셋 메서드
+    """
     st.session_state.app_mode = False
-    st.session_state.round = 0
     st.session_state.viewing_history = False
-    st.session_state.loaded_debate_id = None
+    st.session_state.loaded_question_id = None
     st.session_state.docs = {}
-
-
-def set_debate_to_state(topic, messages, debate_id, docs):
-    st.session_state.app_mode = True
-    st.session_state.messages = messages
-    st.session_state.viewing_history = True
-    st.session_state.debate_topic = topic
-    st.session_state.loaded_debate_id = debate_id
-    st.session_state.docs = docs
